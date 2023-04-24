68a7123bfb57e26fa0c07a05796715d08a87f29e
jose victor garcia llorente <victorgarciallorente20@gmail.com>
2023-04-24 20:08:38 +0200

    Cambios en clases: Combate-Ronda-Vampiro-Personaje

--- Practica MP 2/src/practica/mp/pkg2/Ronda.java
+++ Practica MP 2/src/practica/mp/pkg2/Ronda.java
@@ -4,10 +4,48 @@
  */
 package practica.mp.pkg2;
 
+import java.lang.*;
+
 /**
  *
  * @author Alex
  */
 public class Ronda {
-    
+    private String combate;
+    private Personaje personaje1, personaje2;
+    private int numRonda;
+    private Personaje ganador;   //Para guardar al ganador de la ronda
+
+    public Ronda (Combate c){                       
+        this.combate = c.getID(); //Se puede poner como un puntero que apunta al combate principal, ya que en esta clase Ronda no modificaremos detalles del combate. Y Si lo hacemos, lo modificaremos en el combate original, mediante this.combate.
+        this.personaje1 = c.getPersonaje1();
+        this.personaje2 = c.getPersonaje2();
+        this.numRonda = c.getRondas().size() + 1;
+        this.ganador = null;
+    }
+
+    public Personaje getGanador(){
+        return this.ganador;
+    }
+
+    public void calculoGanadorRonda(){              //Asigna el ganador de la ronda                            
+        int p1; int p2;
+        p1 = calculoNumExitos(this.personaje1);
+        p2 = calculoNumExitos(this.personaje2);
+        if (p1>p2) {this.ganador = this.personaje1;}             //Si el numero de exitos fuera igual en los dos personajes, el ganador seria null. Y al calcular a cual de los dos se le resta salud, no seria ninguno porque el metodo equals devolveria false
+        else {this.ganador = this.personaje2;}
+    }
+
+    //calculoPotencialAtaque(){} no puede ir en esta clase. Debe ir en
+
+    public int calculoNumExitos(Personaje p){
+        int total = 0;
+        for(int i=1;i<=p.getPotencialAtaque();i++){        
+            int n = (((int)Math.random()*10)%6)+1;      //Calcula un numero random entre 1 y 6
+            if ((n==5) || (n==6)) {                       //Si el numero random es 5 o 6, sumamos 1 al valor total del ataque.
+                total = total + 1;
+            }
+        }
+        return total;
+    }
 }
--- Practica MP 2/src/practica/mp/pkg2/Vampiro.java
+++ Practica MP 2/src/practica/mp/pkg2/Vampiro.java
@@ -10,4 +10,16 @@ package practica.mp.pkg2;
  */
 public class Vampiro extends Personaje {
     private int sangre;
+
+    public Vampiro(String s){
+        super(s);
+        this.sangre = 5;            //NOTA: Hay que inicializar el valor de la sangre a algun numero. No se puede quedar como constante 5
+    }
+
+    @Override
+    public void setPotencialAtaque(){
+        int potencial = this.sangre + super.getPoder();     //Aqui habria que anadir el valor de ataque de su disciplina, equipo etc.
+        super.setPotencialAtaque(potencial);                //Tambien habria que anadir el correspondiente calculo del potencial de ataque en las clases que extienden (heredan) de personaje
+    }
+
 }
