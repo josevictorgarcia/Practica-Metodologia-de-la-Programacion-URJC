4eebbede6ce21263433d5e3a2338a3678fb0a122
Marlex <130141193+alejandromartincarrera@users.noreply.github.com>
2023-05-05 12:46:56 +0200

    OperadorSistemaTest y PersonajeTest

--- Practica MP 2/test/practica/mp/pkg2/OperadorSistemaTest.java
+++ Practica MP 2/test/practica/mp/pkg2/OperadorSistemaTest.java
@@ -16,11 +16,47 @@ public class OperadorSistemaTest {
     public OperadorSistemaTest() {
     }
 
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+
     @Test
     public void testEditarPersonaje() {
         System.out.println("editarPersonaje");
         App app = new App();
-        Ranking rank = app.loadRanking();
+        Ranking rank = new Ranking();
+        
+        //crear cliente
+        Menu menu = new Menu();
+        GeneradorIDs generador= new GeneradorIDs();
+        Cliente cliente = new Cliente("cliente", "cliente","contra", menu, generador);
+        
+        Licantropo personaje1= new Licantropo("prueba", new Don("prueba",1,2,1));
+        personaje1.setRabia(0);
+        personaje1.setPoder(3);
+        Arma arma = new Arma("arma",1,0,0,2);
+        personaje1.anadirItem(arma);
+        personaje1.ponerArmaActiva(arma);
+        Armadura armadura = new Armadura("armadura", 1,0,0);
+        personaje1.anadirItem(armadura);
+        personaje1.setArmadura_activa(armadura);
+        
+        cliente.setPersonaje(personaje1);
+        
+        rank.añadirUsuario(cliente);
+        
         OperadorSistema instance = new OperadorSistema("prueba", "prueba", "prueba", new Menu());
         instance.editarPersonaje(rank);
     }
@@ -28,49 +64,121 @@ public class OperadorSistemaTest {
     @Test
     public void testCompletarPersonaje() {
         System.out.println("completarPersonaje");
-        Ranking rank = null;
-        Equipos equipos = null;
-        OperadorSistema instance = null;
+        App app = new App();
+        Ranking rank = new Ranking();
+        
+        //crear cliente
+        Menu menu = new Menu();
+        GeneradorIDs generador= new GeneradorIDs();
+        Cliente cliente = new Cliente("cliente", "cliente","contra", menu, generador);
+        
+        Licantropo personaje1= new Licantropo("prueba", new Don("prueba",1,2,1));
+        personaje1.setRabia(0);
+        personaje1.setPoder(3);
+        Arma arma = new Arma("arma",1,0,0,2);
+        personaje1.anadirItem(arma);
+        personaje1.ponerArmaActiva(arma);
+        Armadura armadura = new Armadura("armadura", 1,0,0);
+        personaje1.anadirItem(armadura);
+        personaje1.setArmadura_activa(armadura);
+        
+        cliente.setPersonaje(personaje1);
+        
+        rank.añadirUsuario(cliente);
+        
+        
+        Equipos equipos = new Equipos();
+        equipos.anadirEquipo(new Arma("arma2",1,0,1,2));
+        OperadorSistema instance = new OperadorSistema("op", "op", "contra", menu);
         instance.completarPersonaje(rank, equipos);
     }
 
     @Test
     public void testValidarDesafios() {
         System.out.println("validarDesafios");
-        Ranking ranking = null;
-        OperadorSistema instance = null;
-        instance.validarDesafios(ranking);
+        
+        Menu menu = new Menu();
+        GeneradorIDs generador= new GeneradorIDs();
+        Cliente instance = new Cliente("cliente", "cliente","contra", menu, generador);
+        Cliente cliente2= new Cliente ("cliente2","cliente2", "contra",menu,generador);
+        //crear personajes:
+        Licantropo personaje1= new Licantropo("prueba", new Don("prueba",1,2,1));
+        personaje1.setRabia(0);
+        personaje1.setPoder(3);
+        Arma arma = new Arma("arma",1,0,0,2);
+        personaje1.anadirItem(arma);
+        personaje1.ponerArmaActiva(arma);
+        Armadura armadura = new Armadura("armadura", 1,0,0);
+        personaje1.anadirItem(armadura);
+        personaje1.setArmadura_activa(armadura);
+        
+        Licantropo personaje2= new Licantropo("prueba", new Don("prueba",2,1,1));
+        personaje2.setRabia(0);
+        personaje2.setPoder(3);
+        Arma arma2 = new Arma("arma",2,0,0,2);
+        personaje2.anadirItem(arma2);
+        personaje2.ponerArmaActiva(arma2);
+        Armadura armadura2 = new Armadura("armadura", 1,0,0);
+        personaje2.anadirItem(armadura2);
+        personaje2.setArmadura_activa(armadura2);
+        
+        instance.setPersonaje(personaje1);
+        instance.setPersonaje(personaje2);
+        
+        
+        Ranking ranking = new Ranking();
+        ranking.añadirUsuario(instance);
+        ranking.añadirUsuario(cliente2);
+        
+        Desafio des = new Desafio(instance.getPersonaje(), cliente2.getPersonaje(), 2, generador, instance);
+        instance.enviarDesafio(des, cliente2);
+        
+        
+        OperadorSistema op = new OperadorSistema("op", "op", "contra", menu);
+        op.validarDesafios(ranking);
     }
 
     @Test
     public void testBanear() {
         System.out.println("banear");
-        Ranking ranking = null;
-        OperadorSistema instance = null;
+        Ranking ranking = new Ranking();
+        Menu menu = new Menu();
+        GeneradorIDs generador = new GeneradorIDs();
+        Cliente cliente = new Cliente("cliente", "cliente", "contra", menu, generador);
+        ranking.añadirUsuario(cliente);
+        OperadorSistema instance = new OperadorSistema("op", "op", "contra", menu);
         instance.banear(ranking);
+        assertTrue(cliente.isBaneado());
     }
 
     @Test
     public void testDesbanear() {
         System.out.println("desbanear");
-        Ranking ranking = null;
-        OperadorSistema instance = null;
+        Ranking ranking = new Ranking();
+        Menu menu = new Menu();
+        GeneradorIDs generador = new GeneradorIDs();
+        Cliente cliente = new Cliente("cliente", "cliente", "contra", menu, generador);
+        cliente.setBaneado(true);
+        ranking.añadirUsuario(cliente);
+        OperadorSistema instance = new OperadorSistema("op", "op", "contra", menu);
         instance.desbanear(ranking);
+        assertTrue(! cliente.isBaneado());
     }
 
     @Test
     public void testAñadirItemTienda() {
         System.out.println("a\u00f1adirItemTienda");
-        Equipos equipos = null;
-        OperadorSistema instance = null;
+        Equipos equipos = new Equipos();
+        Menu menu = new Menu();
+        OperadorSistema instance = new OperadorSistema("op", "op", "contra", menu);
         instance.añadirItemTienda(equipos);
     }
 
     @Test
     public void testAñadirHabilidad() throws Exception {
         System.out.println("a\u00f1adirHabilidad");
-        Habilidades habilidades = null;
-        OperadorSistema instance = null;
+        Habilidades habilidades = new Habilidades();
+        OperadorSistema instance = new OperadorSistema("op", "op", "contra", menu);
         instance.añadirHabilidad(habilidades);
     }
     
---
+++ Practica MP 2/test/practica/mp/pkg2/PersonajeTest.java
@@ -0,0 +1,216 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package practica.mp.pkg2;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Alex
+ */
+public class PersonajeTest {
+    
+    public PersonajeTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of anadirItem method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testAnadirItem() {
+        System.out.println("anadirItem");
+        Equipo item = new Arma("arma", 1, 0, 0, 2);
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        instance.anadirItem(item);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of modificarOro method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testModificarOro() {
+        System.out.println("modificarOro");
+        int cant = 0;
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        instance.modificarOro(cant);
+        // TODO review the generated test code and remove the default call to fail.
+        assertTrue(instance.getOro()==cant);
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of ponerArmaActiva method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testPonerArmaActiva() {
+        System.out.println("ponerArmaActiva");
+        Arma arma = new Arma("arma",1,0,1,2);
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        instance.ponerArmaActiva(arma);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        assertTrue(instance.getArmas_activas().get(0).getNombre().equals(arma.getNombre())); //si el nombre del primer arma activa coincide con el del arma que acabamos de poner como activa
+    }
+
+    /**
+     * Test of oroSuficiente method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testOroSuficiente() {
+        System.out.println("oroSuficiente");
+        int oro = 5;
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        boolean expResult = true;
+        boolean result = instance.oroSuficiente(oro); //por defecto tiene 50 de oro
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of añadirModificador method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testAñadirModificador() {
+        System.out.println("a\u00f1adirModificador");
+        int ataque =2;
+        Modificador mod = new Modificador("mod", ataque, true);
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        instance.añadirModificador(mod);
+        
+        assertTrue(instance.calcularModificadoresAtaque()== ataque); 
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of añadirEsbirro method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testAñadirEsbirro() {
+        System.out.println("a\u00f1adirEsbirro");
+        Esbirro es = new Humano("humano", 1, Lealtad.ALTA);
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        instance.añadirEsbirro(es);
+        
+        assertTrue(instance.getEsbirros().size() >0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularSaludEsbirros method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testCalcularSaludEsbirros() {
+        System.out.println("calcularSaludEsbirros");
+        int salud=1;
+        Esbirro es = new Humano("humano", salud, Lealtad.ALTA);
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        instance.añadirEsbirro(es);
+        instance.calcularSaludEsbirros();
+        
+        assertTrue(instance.getSalud_esbirros()==salud);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularAtaqueEquipo method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testCalcularAtaqueEquipo() {
+        System.out.println("calcularAtaqueEquipo");
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        int ataque=2;
+        Arma arma = new Arma("arma", ataque, 0, 1, 2);
+        instance.ponerArmaActiva(arma);
+        int expResult = ataque;
+        int result = instance.calcularAtaqueEquipo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularModificadoresAtaque method, of class Personaje.
+     */
+    @org.junit.jupiter.api.Test
+    public void testCalcularModificadoresAtaque() {
+        System.out.println("calcularModificadoresAtaque");
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        int ataque=2;
+        instance.añadirModificador(new Modificador("mod", ataque, true));
+        int expResult = ataque;
+        int result = instance.calcularModificadoresAtaque();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testCalcularModificadoresDefensa() {
+        System.out.println("calcularModificadoresDefensa");
+        Personaje instance = new Licantropo("lican", new Don("don", 1,1,1));
+        int defensa=2;
+        instance.añadirModificador(new Modificador("mod", defensa, false));
+        int expResult = defensa;
+        int result = instance.calcularModificadoresDefensa();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    
+   /* son metodos a los que se les hace override
+    @org.junit.jupiter.api.Test
+    public void testGetPotencialDefensa() {
+        
+    }
+*/
+
+    
+    
+}
